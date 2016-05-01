ed8941b56654363f1d9d93b1d87b1bdce25bc97a
wjdddj <wj.dddj@gmail.com>
2016-05-01 15:57:18 -0700

    add one more senario

--- cal24_simple.R
+++ cal24_simple.R
@@ -2,9 +2,10 @@
 ## This function aims to solve the 24 point problem with all possible solutions
 ## algorithm:
 ##      brute force enumerate all possibilities
-##      this problem can be solved in two senarios: 
+##      this problem can be solved in three senarios: 
 ##        1. (((a op b) op c) op d)
 ##        2. (a op b) op (b op d)
+##        3. (a op (b op c)) op d
 ## 
 ## limitations: 
 ##      This function does not consider duplicated numbers or cumulative/associative laws
@@ -18,18 +19,18 @@ cal24_simple <- function(numbers){
   m <- nrow(enum_op)
   
   solutions <- list()
-  # this problem can be solved in two senarios: 
+  perm_number <- alply(permutations(4, 4), 1)
+  perm_number <- llply(perm_number, function(x)numbers[x])
+  
+  # this problem can be solved in three senarios: 
   ## 1. (((a op b) op c) op d)
   for(i in 1:m){
     select_op <- operator[enum_op[i, ]]
-    perm_number <- alply(permutations(4, 4), 1)
-    perm_number <- llply(perm_number, function(x)numbers[x])
     result <- llply(perm_number, function(x){
       Formula <- paste("(", "(", x[1], select_op[1], x[2], ")", select_op[2], x[3], ")", select_op[3], x[4])
-      value <- eval(parse(text=Formula))
+      value <- eval(parse(text = Formula))
       list(Formula = Formula, value = value)
     })
-    
     solutions_inLoop <- llply(result, function(x){
       if(is.finite(x$value)){
         if(x$value == 24)
@@ -43,14 +44,11 @@ cal24_simple <- function(numbers){
   ## 2. (a op b) op (b op d)
   for(i in 1:m){
     select_op <- operator[enum_op[i, ]]
-    perm_number <- alply(permutations(4, 4), 1)
-    perm_number <- llply(perm_number, function(x)numbers[x])
     result <- llply(perm_number, function(x){
       Formula <- paste("(", x[1], select_op[1], x[2], ")", select_op[2], "(", x[3], select_op[3], x[4], ")")
-      value <- eval(parse(text=Formula))
+      value <- eval(parse(text = Formula))
       list(Formula = Formula, value = value)
     })
-    
     solutions_inLoop <- llply(result, function(x){
       if(is.finite(x$value)){
         if(x$value == 24)
@@ -60,6 +58,25 @@ cal24_simple <- function(numbers){
     solutions_inLoop[sapply(solutions_inLoop, is.null)] <- NULL
     solutions <- c(solutions, solutions_inLoop)
   }
+  
+  ## 3. (a op (b op c)) op d
+  for(i in 1:m){
+    select_op <- operator[enum_op[i, ]]
+    result <- llply(perm_number, function(x){
+      Formula <- paste("(", x[1], select_op[1], '(', x[2], select_op[2], x[3], ")", ")", select_op[3], x[4])
+      value <- eval(parse(text = Formula))
+      list(Formula = Formula, value = value)
+    })
+    solutions_inLoop <- llply(result, function(x){
+      if(is.finite(x$value)){
+        if(x$value == 24)
+          x$Formula
+      }
+    })
+    solutions_inLoop[sapply(solutions_inLoop, is.null)] <- NULL
+    solutions <- c(solutions, solutions_inLoop)
+  }
+  
   if(length(solutions) != 0)
     names(solutions) <- paste('solution', 1:length(solutions), sep = '_')
   
